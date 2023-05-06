14396d3d0b1c3218c1d2ce9d769b1fa77457505e
Shivam singh <129679636+ShivamISAvailable@users.noreply.github.com>
2023-05-06 10:19:01 -0700

    Create print.py

---
+++ print.py
@@ -0,0 +1,10 @@
+a=(int(input("Enter a first number: ")
+a=(int(input("Enter a second number: ")
+c=a+b
+print("Add is "c)
+c=a-b
+print("Sub is "c)
+c=a*b
+print("Mul is "c)
+c=a/b
+print("Div is "c)
